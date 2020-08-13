c5b72fe395e751ce5ff8096d989e57ec085801ef
alexthehaszard <aggis.haszard@gmail.com>
2020-08-13 21:38:30 +1200

    ➕ - add neural network

--- bird.js
+++ bird.js
@@ -1,24 +1,50 @@
 class Bird {
-  constructor() {
+  constructor(n) {
+    this.setup();
+
+    if (n) this.nn = n;
+    else this.nn = new NeuralNetwork(2, 4, 2);
+  }
+
+  setup() {
     this.x = 80;
     this.y = 350;
-    
+
     this.velocity = 0;
     this.gravity = 0.6;
     this.lift = 10;
+    this.score = 0;
+
+    this.canMove = true;
   }
-  
+
+  willJump() {
+    const p = currentPipe % 2 === 1 ? pipe[index] : pipe2[index2];
+    const prediction = this.nn.predict([this.y, p.y + 40]);
+    if (prediction[0] > prediction[1]) this.jump();
+  }
+
   update() {
+    if (!this.canMove) {
+      return;
+    }
+    this.score++;
+    this.willJump();
     this.y -= this.velocity;
     this.velocity -= this.gravity;
   }
-  
+
   jump() {
-    this.velocity = this.lift;
+    if (this.canMove) this.velocity = this.lift;
   }
-  
+
   show() {
     rectMode(CENTER);
+    fill(255, 255, 255, 50);
     rect(this.x, this.y, 35, 30);
+  }
+
+  m() {
+    this.nn.mutate(1);
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-let bird;
+let bird = [];
 let pipe;
 let pipe2;
 let gameStarted;
@@ -7,12 +7,17 @@ let indexUsed;
 let index2;
 let index2Used;
 let score;
-let canMove;
 let stopped;
+let currentPipe = 1;
+let generation = 0;
+let currentScore = 0;
+
+for (let i = 0; i < 100; i++) {
+  bird[i] = new Bird();
+}
 
 function setup() {
   createCanvas(400, 600);
-  bird = new Bird();
   pipe = [new Pipe()];
   pipe2 = [new Pipe()];
   gameStarted = false;
@@ -22,21 +27,23 @@ function setup() {
   index2Used = -1;
   score = 0;
   stopped = false;
-  canMove = true;
   pipe[0].createPipe();
   pipe2[0].createPipe();
   pipe2[0].x = 625;
 }
 
 function draw() {
-  if (stopped) return;
-  background(220);
+  if (stopped) {
+    return;
+  }
+  background(0);
   fill(255);
-  if (!canMove) fill(255, 0, 0);
-  bird.show();
+  for (let i = 0; i < bird.length; i++) {
+    if (bird[i].canMove) bird[i].show();
+    if (gameStarted) bird[i].update();
+  }
   if (gameStarted == true) {
     fill(255);
-    bird.update();
     pipe[index].update();
     pipe2[index2].update();
   }
@@ -55,52 +62,78 @@ function draw() {
     pipe2[index2].createPipe();
   }
 
-  if (bird.x > pipe[index].x && indexUsed != index) {
-    indexUsed = index;
-    score++;
+  let isDead = true;
+  for (let i = 0; i < bird.length; i++) {
+    if (bird[i].canMove === true) {
+      isDead = false;
+    }
   }
-
-  if (bird.x > pipe2[index2].x && index2Used != index2) {
-    index2Used = index2;
-    score++;
-  }
-
-  if (bird.y >= height) {
+  if (isDead === true) {
     stopped = true;
-    canMove = false;
+    genUp();
+    return;
   }
 
-  fill(0);
-  textAlign(CENTER);
-  textSize(100);
-  text(score, width / 2, 100);
+  for (let i = 0; i < bird.length; i++) {
+    if (bird[i].x > pipe[index].x && indexUsed != index) {
+      indexUsed = index;
+      bird[i].score++;
+      currentScore++;
+      setTimeout(() => {
+        currentPipe++;
+        console.log(currentPipe);
+      }, 800);
+    }
 
-  if (
-    bird.y + 25 < pipe[index].y - pipe[index].gap / 2 &&
-    bird.x + 20 > pipe[index].x &&
-    bird.x - 20 < pipe[index].x + 50
-  ) {
-    canMove = false;
-  } else if (
-    bird.y - 30 > pipe[index].y + pipe[index].gap / 2 &&
-    bird.x + 20 > pipe[index].x &&
-    bird.x - 20 < pipe[index].x + 50
-  ) {
-    canMove = false;
-  }
+    if (bird[i].x > pipe2[index2].x && index2Used != index2) {
+      index2Used = index2;
+      bird[i].score++;
+      currentScore++;
+      setTimeout(() => {
+        currentPipe++;
+        console.log(currentPipe);
+      }, 800);
+    }
 
-  if (
-    bird.y + 25 < pipe2[index2].y - pipe2[index2].gap / 2 &&
-    bird.x + 20 > pipe2[index2].x &&
-    bird.x - 20 < pipe2[index2].x + 50
-  ) {
-    canMove = false;
-  } else if (
-    bird.y - 30 > pipe2[index2].y + pipe2[index2].gap / 2 &&
-    bird.x + 20 > pipe2[index2].x &&
-    bird.x - 20 < pipe2[index2].x + 50
-  ) {
-    canMove = false;
+    if (bird[i].y >= height) {
+      bird[i].canMove = false;
+    }
+
+    fill(255);
+    stroke(0);
+    textAlign(CENTER);
+    textSize(100);
+    text(currentScore, width / 2, 100);
+    textSize(30);
+    text(`Generation: ${generation}`, width / 2, 150);
+
+    if (
+      bird[i].y + 25 < pipe[index].y - pipe[index].gap / 2 &&
+      bird[i].x + 20 > pipe[index].x &&
+      bird[i].x - 20 < pipe[index].x + 50
+    ) {
+      bird[i].canMove = false;
+    } else if (
+      bird[i].y - 30 > pipe[index].y + pipe[index].gap / 2 &&
+      bird[i].x + 20 > pipe[index].x &&
+      bird[i].x - 20 < pipe[index].x + 50
+    ) {
+      bird[i].canMove = false;
+    }
+
+    if (
+      bird[i].y + 25 < pipe2[index2].y - pipe2[index2].gap / 2 &&
+      bird[i].x + 20 > pipe2[index2].x &&
+      bird[i].x - 20 < pipe2[index2].x + 50
+    ) {
+      bird[i].canMove = false;
+    } else if (
+      bird[i].y - 30 > pipe2[index2].y + pipe2[index2].gap / 2 &&
+      bird[i].x + 20 > pipe2[index2].x &&
+      bird[i].x - 20 < pipe2[index2].x + 50
+    ) {
+      bird[i].canMove = false;
+    }
   }
   if (stopped || !gameStarted) {
     textSize(35);
@@ -109,13 +142,31 @@ function draw() {
 }
 
 function keyPressed() {
-  if (bird.y <= 0) return;
-  if (!canMove && stopped) {
+  if (stopped) {
     setup();
     stopped = false;
   }
-  if (canMove) {
-    bird.jump();
-  }
   gameStarted = true;
+}
+
+function genUp() {
+  bird.sort(compare);
+  bird.reverse();
+  console.log(bird);
+  setTimeout(() => {
+    for (let i = 0; i < bird.length / 2; i++) {
+      bird[i].setup();
+      bird.splice(i + bird.length / 2 - 1, 1);
+      bird.push(new Bird(bird[i].nn.copy()));
+      bird[bird.length - 1].m();
+    }
+    keyPressed();
+    currentPipe = 1;
+    generation++;
+    currentScore = 0;
+  }, 1000);
+}
+
+function compare(a, b) {
+  return a.score - b.score;
 }
